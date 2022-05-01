6961670034abc11787521b6ecae3b29226f1492f
yaboliudotug <yaboliu_ug@gmail.com>
2022-05-01 12:15:24 +0800

    prepare datasets

--- examples/domain_adaptation/object_detection/prepare_datasets.sh
+++ examples/domain_adaptation/object_detection/prepare_datasets.sh
@@ -1,2 +1,1 @@
-ln -s /disk/liuyabo/data/daod/cityscapes/cityscapes_in_voc /disk/liuyabo/research/Transfer-Learning-Library/examples/domain_adaptation/object_detection/datasets/cityscapes_in_voc
-ln -s /disk/liuyabo/data/daod/cityscapes/cityscapes_in_voc /disk/liuyabo/research/Transfer-Learning-Library/examples/domain_adaptation/object_detection/datasets/cityscapes_in_voc
+ln -s /disk/liuyabo/data/daod /disk/liuyabo/research/Transfer-Learning-Library/examples/domain_adaptation/object_detection/datasets
