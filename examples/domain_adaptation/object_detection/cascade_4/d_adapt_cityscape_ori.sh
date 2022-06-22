edf1fd64fa4f033d6b3e04ba5b224ff25da03d46
yaboliudotug <yaboliu.ug@gmail.com>
2022-06-22 23:01:28 +0800

    test

--- examples/domain_adaptation/object_detection/cascade_4/d_adapt_cityscape_ori.sh
+++ examples/domain_adaptation/object_detection/cascade_4/d_adapt_cityscape_ori.sh
@@ -63,21 +63,21 @@
 # # ResNet101 Based Faster RCNN: Cityscapes -> Foggy Cityscapes
 # # 40.1
 # pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy/model_final.pth
-pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy_cache/model_0029999.pth
-CUDA_VISIBLE_DEVICES=2 python d_adapt.py --num-gpus 1 --workers-c 4 --max-train-c 20 --ignored-scores-c 0.05 0.5   \
-  --config-file config/faster_rcnn_R_101_C4_cityscapes.yaml --use-pre-cache 0 --update-score 0 --update-proposal 0 \
-  -s Cityscapes ../datasets/cityscapes_in_voc -t FoggyCityscapes ../datasets/foggy_cityscapes_in_voc/  \
-  --test FoggyCityscapesTest ../datasets/foggy_cityscapes_in_voc/ --finetune --trade-off 0.5 --bbox-refine \
-  OUTPUT_DIR logs/faster_rcnn_R_101_C4/cityscapes2foggy/phase1 MODEL.WEIGHTS ${pretrained_models} SEED 0
-# --confidence-ratio-c 0.05 能够达到40.6map
- #--confidence-ratio-c 0.05 \
-
-#  pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy_cache/model_0029999.pth
-# CUDA_VISIBLE_DEVICES=1 python d_adapt.py --num-gpus 1 --workers-c 4 --max-train-c 20 --ignored-scores-c 0.05 0.5  \
+# pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy_cache/model_0029999.pth
+# CUDA_VISIBLE_DEVICES=0 python d_adapt.py --num-gpus 1 --workers-c 4 --max-train-c 20 --ignored-scores-c 0.05 0.5   \
 #   --config-file config/faster_rcnn_R_101_C4_cityscapes.yaml --use-pre-cache 0 --update-score 0 --update-proposal 0 \
 #   -s Cityscapes ../datasets/cityscapes_in_voc -t FoggyCityscapes ../datasets/foggy_cityscapes_in_voc/  \
 #   --test FoggyCityscapesTest ../datasets/foggy_cityscapes_in_voc/ --finetune --trade-off 0.5 --bbox-refine \
-#   OUTPUT_DIR logs/faster_rcnn_R_101_C4/cityscapes2foggy_test_6/phase1 MODEL.WEIGHTS ${pretrained_models} SEED 0
+#   OUTPUT_DIR logs/faster_rcnn_R_101_C4/cityscapes2foggy/phase1 MODEL.WEIGHTS ${pretrained_models} SEED 0
+# --confidence-ratio-c 0.05 能够达到40.6map
+ #--confidence-ratio-c 0.05 \
+
+pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy_cache/model_0029999.pth
+CUDA_VISIBLE_DEVICES=0 python d_adapt.py --num-gpus 1 --workers-c 4 --max-train-c 20 --ignored-scores-c 0.05 0.5  \
+  --config-file config/faster_rcnn_R_101_C4_cityscapes.yaml --use-pre-cache 1 --update-score 0 --update-proposal 0 \
+  -s Cityscapes ../datasets/cityscapes_in_voc -t FoggyCityscapes ../datasets/foggy_cityscapes_in_voc/  \
+  --test FoggyCityscapesTest ../datasets/foggy_cityscapes_in_voc/ --finetune --trade-off 0.5 --bbox-refine \
+  OUTPUT_DIR logs/faster_rcnn_R_101_C4/cityscapes2foggy_2/phase1 MODEL.WEIGHTS ${pretrained_models} SEED 0
 
 # # 42.4
 # pretrained_models=logs/faster_rcnn_R_101_C4/cityscapes2foggy/phase1/model_final.pth
--- examples/domain_adaptation/object_detection/source_only.sh
+++ examples/domain_adaptation/object_detection/source_only.sh
@@ -13,7 +13,7 @@
 #   OUTPUT_DIR logs/source_only/faster_rcnn_R_101_C4/voc2watercolor_comic MODEL.ROI_HEADS.NUM_CLASSES 6
 
 # ResNet101 Based Faster RCNN: Cityscapes->Foggy Cityscapes
-CUDA_VISIBLE_DEVICES=2,3 python source_only.py  --num-gpus 2 \
+CUDA_VISIBLE_DEVICES=0,1 python source_only.py  --num-gpus 2 \
   --config-file config/faster_rcnn_R_101_C4_cityscapes.yaml \
   -s Cityscapes datasets/cityscapes_in_voc/ -t FoggyCityscapes datasets/foggy_cityscapes_in_voc \
   --test CityscapesTest datasets/cityscapes_in_voc/ FoggyCityscapesTest datasets/foggy_cityscapes_in_voc --finetune \
