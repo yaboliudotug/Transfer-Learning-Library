c2b5126443c50f4244d71208c44dcd676ff33ed9
yaboliudotug <yaboliu_ug@gmail.com>
2022-06-22 16:55:39 +0800

    test

--- examples/domain_adaptation/object_detection/cascade_4/d_adapt_cityscape_ori_test.sh
+++ examples/domain_adaptation/object_detection/cascade_4/d_adapt_cityscape_ori_test.sh
@@ -66,12 +66,12 @@
 pretrained_models=../logs/source_only/faster_rcnn_R_101_C4/cityscapes2foggy_cache/model_0029999.pth
 CUDA_VISIBLE_DEVICES=1 python d_adapt.py --num-gpus 1 --workers-c 4 --max-train-c 20 --ignored-scores-c 0.05 0.5  --confidence-ratio-c 0.025 \
   --config-file config/faster_rcnn_R_101_C4_cityscapes.yaml --use-pre-cache 1 --update-score 0 --update-proposal 1 \
-  --ignored-ious-ls [[0.4,0.5]] \
   -s Cityscapes ../datasets/cityscapes_in_voc -t FoggyCityscapes ../datasets/foggy_cityscapes_in_voc/  \
   --test FoggyCityscapesTest ../datasets/foggy_cityscapes_in_voc/ --finetune --trade-off 0.5 --bbox-refine \
   OUTPUT_DIR logs/faster_rcnn_R_101_C4/cityscapes2foggy_update_test/phase1 MODEL.WEIGHTS ${pretrained_models} SEED 0
 # --confidence-ratio-c 0.05 能够达到40.6map
  #--confidence-ratio-c 0.05 \
+ # --ignored-ious-ls [[0.4,0.5]] \
 
 # # 42.4
 # pretrained_models=logs/faster_rcnn_R_101_C4/cityscapes2foggy/phase1/model_final.pth
